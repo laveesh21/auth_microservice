b1bc2e8f8711776c5ddd24aceccae25f7137f331
laveesh21 <laveeshtomar.royalking@gmail.com>
2025-12-10 23:47:51 +0530

    project init

---
+++ src/main/java/com/apexauth/apexauth/ApexauthApplication.java
@@ -0,0 +1,13 @@
+package com.apexauth.apexauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApexauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApexauthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apexauth/apexauth/ApexauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apexauth.apexauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApexauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
